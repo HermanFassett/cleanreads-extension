111c500c05a49c34950c82196bec9c9c9b469a01
Herman Fassett <hermanfassett@gmail.com>
2022-01-18 05:20:30 -0800

    Omit credentials on scraping

--- src/pages/Background/goodreads.js
+++ src/pages/Background/goodreads.js
@@ -4,7 +4,7 @@ import { parseBookHTML } from '../Common/cleanreads';
 
 // Scrape book data from goodreads website given (gr) book id
 export const getBook = async (id) => {
-	const response = await fetch(`https://www.goodreads.com/book/show/${id}`);
+	const response = await fetch(`https://www.goodreads.com/book/show/${id}`, { credentials: 'omit' });
 	const html = await response.text();
 	return await parseBookHTML(html);
 };
@@ -13,7 +13,7 @@ export const getBook = async (id) => {
 export const getGroupShelf = async (id, page, books) => {
 	if (!page) page = 1;
 	if (!books) books = [];
-	const response = await fetch(`https://www.goodreads.com/group/bookshelf/${id}?per_page=100&page=${page}&utf8=✓&view=covers`);
+	const response = await fetch(`https://www.goodreads.com/group/bookshelf/${id}?per_page=100&page=${page}&utf8=✓&view=covers`, { credentials: 'omit' });
 	const html = await response.text();
 	const $ = cheerio.load(html);
 	const pageCount = +$([...$($('.next_page').parent()).find('a:not(.next_page)')].pop()).text();
@@ -32,7 +32,7 @@ export const getGroupShelf = async (id, page, books) => {
 export const getList = async (id, page, books) => {
 	if (!page) page = 1;
 	if (!books) books = [];
-	const response = await fetch(`https://www.goodreads.com/list/show/${id}?page=${page}`);
+	const response = await fetch(`https://www.goodreads.com/list/show/${id}?page=${page}`, { credentials: 'omit' });
 	const html = await response.text();
 	const $ = cheerio.load(html);
 	const pageCount = +$([...$('.pagination a:not(.next_page)')].pop()).text();
